7602d97e605697745362093897140743b7115f50
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-23 14:20:01 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -7,18 +7,4 @@ export NEZHA_KEY="N9BVoBfucVIrIzCB00"
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
 
-
-generate_autodel() {
-  cat > delete.sh <<EOF
-while true; do
-  rm -rf /app/.git
-  sleep 30
-done
-EOF
-}
-
-generate_autodel
-
-[ -e delete.sh ] && bash delete.sh
-
 tail -f /dev/null
